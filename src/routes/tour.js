a21e62fd6e653ca215bbd5e3b3171955a6bb5264
Truong <truonghs2903@gmail.com>
2024-04-07 21:30:42 +0700

    add: setup mongodb connection

--- src/routes/tour.js
+++ src/routes/tour.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const tourController = require('../app/controllers/TourController');
+const tourController = require("../app/controllers/TourController");
 
-router.get('/create', tourController.create);
-router.get('/:id/edit', tourController.edit);
-router.post('/selectall', tourController.selectAll);
-
-router.put('/:id', tourController.update);
-router.delete('/:id', tourController.delete);
-router.get('/:slug', tourController.show);
-router.post('/store', tourController.store);
+// router.get('/create', tourController.create);
+// router.get('/:id/edit', tourController.edit);
+// router.post('/selectall', tourController.selectAll);
+// router.put('/:id', tourController.update);
+// router.delete('/:id', tourController.delete);
+// router.get('/:slug', tourController.show);
+// router.post('/store', tourController.store);
 
 module.exports = router;
