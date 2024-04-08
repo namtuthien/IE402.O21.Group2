41162c4112e99ef2802fda94ecd3927cfba92d96
Truong <truonghs2903@gmail.com>
2024-04-08 15:03:01 +0700

    hotfix: model files

---
+++ src/app/models/line.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const Line = new Schema({
+  _id: { type: ObjectId },
+  points: [
+    {
+      longtitude: { type: String },
+      latitude: { type: String },
+    },
+  ],
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("Line", Line);
---
+++ src/app/models/location.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const Location = new Schema({
+  _id: { type: ObjectId },
+  name: { type: String },
+  region: { type: ObjectId },
+  description: { type: String },
+  activity_list: { type: Array },
+  coordinate: {
+    longtitude: { type: String },
+    latitude: { type: String },
+  },
+  boundary: { type: Array },
+  address: { type: String },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("Location", Location);
