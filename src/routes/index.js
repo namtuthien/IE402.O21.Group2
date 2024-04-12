5ce3884617304626dc1220acce1f670cc54f2490
namtuthien <leducmanh123kt@gmail.com>
2024-04-13 02:22:51 +0700

    add:  site.route, site.controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const tourRouter = require("./tour.route");
+const siteRouter = require("./site.route");
 
 const route = (app) => {
   app.use("/tour", tourRouter);
+  app.use("/", siteRouter);
 };
 
 module.exports = route;
