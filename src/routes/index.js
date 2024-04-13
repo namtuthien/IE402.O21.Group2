5f9c6077444b0224715e019c7517d2aaee05c295
Devbeee <128015666+Devbeee@users.noreply.github.com>
2024-04-13 08:41:29 +0700

    fix: route/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,12 @@ const tourRouter = require("./tour.route");
 
 const authRouter = require('./auth.route');
 
-const route = (app) => {
-  app.use("/tour", tourRouter);
-  app.use("/auth", authRouter);
-
-
 const siteRouter = require("./site.route");
 
 const route = (app) => {
   app.use("/tour", tourRouter);
+  app.use("/auth", authRouter);
   app.use("/", siteRouter);
-
 };
 
 module.exports = route;
