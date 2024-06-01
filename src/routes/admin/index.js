9a74ed1d210842fd71b8b73a69f808de6a91538e
QuocDuy03 <quocduyy.03@gmail.com>
2024-06-02 01:22:01 +0700

    add: delete staff

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const mapRouter = require("./map.route");
+const userRouter = require("./user.route")
 
 router.use("/map", mapRouter);
+router.use("/staff", userRouter);
 
 module.exports = router;
