aa216d5c8a6aefcd3efbbae3469330b54a2eecce
Devbeee <tngb1002@gmail.com>
2024-06-02 18:17:07 +0700

    Add: feature [GET] /customer/rating/store and /staff/rating/get-all

--- src/routes/customer/index.js
+++ src/routes/customer/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const siteRouter = require("./site.route");
+const ratingRouter = require("./rating.route");
 
 router.use("/", siteRouter);
+router.use("/customer", ratingRouter);
 
 module.exports = router;
