907de6c205cd6bcca4d05cf868791e58f68458f5
QuocDuy03 <quocduyy.03@gmail.com>
2024-06-08 19:00:49 +0700

    add: add staff ui

--- src/routes/admin/user.route.js
+++ src/routes/admin/user.route.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const userController = require("../../app/controllers/user.controller")
 
 router.delete('/deleteStaff/:id', userController.deleteUser)
-
+router.get('/addstaff', userController.showAddStaffForm.bind(userController))
 module.exports = router;
