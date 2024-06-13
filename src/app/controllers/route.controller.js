866d2da47b1963432615f44d4d7d999751b07ada
Truong <truonghs2903@gmail.com>
2024-06-13 12:44:38 +0700

    add: tours-map

--- src/app/controllers/route.controller.js
+++ src/app/controllers/route.controller.js
@@ -1,19 +1,47 @@
 const Route = require("../models/route.model");
 const Line = require("../models/line.model");
+const Tour = require("../models/tour.model");
 class RouteController {
-  // [GET] /api/route/getRouteById
-  // async getRouteById(req, res, next) {
-  //   try {
-  //     console.log(req.params._id);
-  //     const routeId = req.params._id;
-  //     const route = await Route.findOne({ _id: req.params._id });
-  //   } catch (err) {
-  //     console.error(err);
-  //     res.status(500).json({
-  //       message: "Internal server error",
-  //     });
-  //   }
-  // }
+  // [GET] /api/route/getTourRoutes
+  async getRouteById(req, res, next) {
+    try {
+      const tours = await Tour.find();
+      let tourRoutes = [];
+
+      for (var i = 0; i < tours.length; i++) {
+        let routeLines = [];
+        let routes = [];
+        let route;
+        let line;
+        for (var j = 0; j < tours[i].routes.length; j++) {
+          let isExist = false;
+          routes.forEach((item) => {
+            if (item._id.toString() === tours[i].routes[j].route.toString()) {
+              console.log(item._id.toString());
+              isExist = true;
+            }
+          });
+          if (!isExist) {
+            route = await Route.findById(tours[i].routes[j].route);
+            line = await Line.findById(route?.lines[0]);
+            routes.push(route._id);
+            line.points.forEach((point) => {
+              routeLines.push([point.longitude, point.latitude]);
+            });
+          }
+        }
+        console.log("-------------------");
+
+        tourRoutes.push({ lines: routeLines, tour: tours[i] });
+      }
+      res.status(200).json({ tourRoutes: tourRoutes, tours });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  }
 }
 
 module.exports = new RouteController();
