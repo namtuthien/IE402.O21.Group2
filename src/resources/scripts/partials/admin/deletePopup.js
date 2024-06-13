7457418a4897f3d01f8a105a9593f2246d64fe8f
seosau <seosausau@gmail.com>
2024-06-13 11:16:21 +0700

    update: add handle request url for delete popup

--- src/resources/scripts/partials/admin/deletePopup.js
+++ src/resources/scripts/partials/admin/deletePopup.js
@@ -5,3 +5,8 @@ document.getElementById('cancelDeleteButton').addEventListener('click', function
 document.getElementById('deletePopupXmarkIcon').addEventListener('click', function() {
     document.getElementById('deletePopupContainer').style.display = 'none';
 })
+
+document.getElementById('submitDeleteButton').addEventListener('click', function() {
+    const url = document.getElementById('handleRequestUrl').innerText;
+    console.log("feth api: ", url);
+})
