2f46ec440069083052a78e24eca0e5f24c4081e8
Truong <truonghs2903@gmail.com>
2024-06-18 12:29:11 +0700

    add: customers table

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const mapRouter = require("./map.route");
 const dashboardRouter = require("./dashboard.route");
 const adminRouter = require("./admin.route");
-const userRouter = require("./user.route")
-const locationRouter = require("./location.route")
+const userRouter = require("./user.route");
+const customerRouter = require("./customer.route");
+
+const locationRouter = require("./location.route");
 router.use("/staff", userRouter);
 router.use("/map", mapRouter);
 router.use("/dashboard", dashboardRouter);
 router.use("/location", locationRouter);
 router.use("/", adminRouter);
+router.use("/customer", customerRouter);
+
 module.exports = router;
