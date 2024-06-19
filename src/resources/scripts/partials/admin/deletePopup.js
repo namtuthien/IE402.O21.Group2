a38e50ac964e90982d7ae12fc2007939fe7ca053
seosau <seosausau@gmail.com>
2024-06-19 10:52:28 +0700

    update: delete popup

--- src/resources/scripts/partials/admin/deletePopup.js
+++ src/resources/scripts/partials/admin/deletePopup.js
@@ -9,4 +9,21 @@ document.getElementById('deletePopupXmarkIcon').addEventListener('click', functi
 document.getElementById('submitDeleteButton').addEventListener('click', function() {
     const url = document.getElementById('handleRequestUrl').innerText;
     console.log("feth api: ", url);
+    if(url.includes("bookings/delete")) {
+        fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert("Xóa thành công!");
+        })
+        .catch(error => {
+            alert("Lỗi khi xóa!")
+        }) 
+    }
+    document.getElementById('deletePopupContainer').style.display = 'none';
+    location.reload();
 })
