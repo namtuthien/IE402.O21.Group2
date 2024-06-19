8c6a20af341b765c5b2d3d5490d0588dd0a39d9b
QuocDuy03 <quocduyy.03@gmail.com>
2024-06-20 00:46:48 +0700

    update: add href link in data action button

--- src/resources/scripts/pages/admin/ratings.js
+++ src/resources/scripts/pages/admin/ratings.js
@@ -76,3 +76,7 @@ singleChecks.forEach((singleCheck) => {
     checkAllElement.checked = allChecked;
   });
 });
+
+const addButton = document.getElementById("add-button");
+
+addButton.classList.add("hidden-important");
--- src/resources/scripts/partials/admin/navbar.js
+++ src/resources/scripts/partials/admin/navbar.js
@@ -10,3 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+const btnLogout = document.getElementById("btn-logout");
+btnLogout.onclick = function (e) {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  };
+
+  fetch("/logout", requestOptions)
+    .then((response) => {
+      console.log(response)
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      console.error("Đăng xuất không thành công:", error);
+    });
+};
