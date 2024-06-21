7faa8a393dc563c309725a1a847cf6798e552b1a
seosau <seosausau@gmail.com>
2024-06-21 18:43:43 +0700

    add: staff view tour

--- src/resources/scripts/pages/admin/bookings.js
+++ src/resources/scripts/pages/admin/bookings.js
@@ -1,2 +1,10 @@
 const addButton = document.getElementById("add-button");
 addButton.classList.add("hidden-important");
+
+if(window.location.href.includes("staff")) {
+    const allViewDetail =  document.getElementsByClassName('bookingsAction-icon');
+    const allStaffViewDetail =  document.getElementsByClassName('bookingsAction-iconStaffUrl');
+    for(let i = 0; i < allViewDetail.length; i++) {
+        allViewDetail[i].href = allStaffViewDetail[i].href;
+    }
+}
--- src/routes/staff/index.js
+++ src/routes/staff/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const StaffController = require("../../app/controllers/staff.controller");
 const RatingController = require("../../app/controllers/rating.controller");
+const BookingController = require("../../app/controllers/booking.controller");
 
 // [GET] one customer
 router.get("/customer/:user_id", StaffController.getCustomerById);
@@ -14,5 +15,10 @@ router.get("/tourguides/location/show", StaffController.showRealTimeLocation);
 // [GET] all ratings
 router.get("/rating/get-all", RatingController.index);
 router.get("/ratings", RatingController.showRatings);
+
+// [GET] get booking
+router.get("/bookings", BookingController.showBookings);
+router.get("/bookings/view/:id", BookingController.showBookingDetail);
+
 
 module.exports = router;
